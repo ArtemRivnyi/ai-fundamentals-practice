964302a7678104294c01391d95016f50cbdde5ed
Artem Rivnyi <123167556+ArtemRivnyi@users.noreply.github.com>
2023-12-14 12:37:14 +0200

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,40 +1,35 @@
 import pandas as pd
 from sklearn.cluster import KMeans, MeanShift
 from sklearn.metrics import silhouette_score
+from sklearn.cluster import estimate_bandwidth
 import matplotlib.pyplot as plt
 
-# Загрузите данные из файла ods
-data_ods = pd.read_excel('C:\\II labs 3\\Lab01\\lab01.ods', engine='odf')
+csv_file_path = 'C:\\II labs 3\\Lab01\\lab01.csv'
 
-# Загрузите данные из файла csv
-data_csv = pd.read_csv('C:\\II labs 3\\Lab01\\lab01.csv', sep=';')
+def load_data(file_path, sep=None):
+    data = pd.read_csv(file_path, sep=sep)
+    data = data.fillna(data.mean())
+    return data
 
-# Объедините два набора данных
-data = pd.concat([data_ods, data_csv])
+def determine_optimal_clusters(data):
+    scores = []
+    for i in range(2, 16):
+        kmeans = KMeans(n_clusters=i, n_init=10, random_state=0).fit(data)
+        score = silhouette_score(data, kmeans.labels_)
+        scores.append(score)
+    optimal_clusters = scores.index(max(scores)) + 2
+    return optimal_clusters, scores
 
-# Замените пропущенные значения на среднее значение каждого столбца
-data = data.fillna(data.mean())
+data = load_data(csv_file_path, sep=';')
 
-# Определите количество кластеров с помощью метода сдвига среднего
-ms = MeanShift()
+bandwidth = estimate_bandwidth(data, quantile=0.15, n_samples=500)
+ms = MeanShift(bandwidth=bandwidth)
 ms.fit(data)
-labels = ms.labels_
 cluster_centers = ms.cluster_centers_
 
-n_clusters_ = len(cluster_centers)
-
-# Оцените score для различных вариантов кластеризации
-scores = []
-for i in range(2, 16):
-    kmeans = KMeans(n_clusters=i, n_init=10, random_state=0).fit(data)
-    score = silhouette_score(data, kmeans.labels_)
-    scores.append(score)
-
-# Проведите кластеризацию методом k-средних с оптимальным количеством кластеров
-optimal_clusters = scores.index(max(scores)) + 2
+optimal_clusters, scores = determine_optimal_clusters(data)
 kmeans = KMeans(n_clusters=optimal_clusters, n_init=10, random_state=0).fit(data)
 
-# Выведите следующие графики
 plt.figure(figsize=(16, 8))
 plt.subplot(221)
 plt.scatter(data.iloc[:, 0], data.iloc[:, 1])
