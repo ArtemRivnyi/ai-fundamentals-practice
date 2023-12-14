0bb49af13808966b52fb62a97f5217412b762a1f
Artem Rivnyi <123167556+ArtemRivnyi@users.noreply.github.com>
2023-12-14 13:28:34 +0200

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,56 +1,50 @@
 import random
-import math
+import numpy as np
+from deap import base, creator, tools, algorithms
 
-def func(x, y, z):
-    return 1 / (1 + (x - 2) ** 2 + (y + 1) ** 2 + (z - 1) ** 2)
+# Функція оцінки
+def eval_func(chromosome):
+    x, y, z = chromosome
+    return 1 / (1 + (x-2)**2 + (y+1)**2 + (z-1)**2),
 
-chrom_len = 3
-pop_size = 100
-gen_num = 100
-interval = (-10, 10)
-mut_prob = 0.01
-def random_ind():
-    return [random.uniform(interval[0], interval[1]) for _ in range(chrom_len)]
-def init_pop():
-    return [random_ind() for _ in range(pop_size)]
-def fitness(ind):
-    return func(*ind)
-def total_fitness(pop):
-    return sum(fitness(ind) for ind in pop)
-def roulette(pop):
-    total = total_fitness(pop)
-    r = random.uniform(0, total)
-    s = 0
-    for ind in pop:
-        s += fitness(ind)
-        if s >= r:
-            return ind
-def crossover(ind1, ind2):
-    point = random.randint(1, chrom_len - 1)
-    ind1[point:], ind2[point:] = ind2[point:], ind1[point:]
-    return ind1, ind2
-def mutate(ind):
-    for i in range(chrom_len):
-        if random.random() < mut_prob:
-            ind[i] = random.uniform(interval[0], interval[1])
-    return ind
-def evolve(pop):
-    new_pop = []
-    best_ind = max(pop, key=fitness)
-    new_pop.append(best_ind)
-    while len(new_pop) < len(pop):
-        ind1 = roulette(pop)
-        ind2 = roulette(pop)
-        ind1, ind2 = crossover(ind1, ind2)
-        ind1 = mutate(ind1)
-        ind2 = mutate(ind2)
-        new_pop.append(ind1)
-        new_pop.append(ind2)
-    return new_pop
-pop = init_pop()
-for gen in range(gen_num):
-    pop = evolve(pop)
-    best_ind = max(pop, key=fitness)
-    print(f"Покоління {gen + 1}: {best_ind}, {fitness(best_ind)}")
-print(f"Максимум функції: {fitness(best_ind)}")
-print(f"Значення x, y, z: {best_ind}")
+# Генетичний алгоритм
+def genetic_algorithm():
+    # Створення типів
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    # Ініціалізація популяції
+    toolbox = base.Toolbox()
+    toolbox.register("attr_float", random.uniform, -10, 10)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=3)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    # Реєстрація генетичних операторів
+    toolbox.register("evaluate", eval_func)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    # Параметри генетичного алгоритму
+    pop_size = 100
+    crossover_prob = 0.7
+    mutation_prob = 0.01
+    num_generations = 100
+
+    # Створення початкової популяції
+    pop = toolbox.population(n=pop_size)
+    hof = tools.HallOfFame(1)
+
+    # Запуск генетичного алгоритму
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", np.mean)
+    stats.register("min", np.min)
+    stats.register("max", np.max)
+
+    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=crossover_prob, mutpb=mutation_prob, ngen=num_generations, stats=stats, halloffame=hof, verbose=True)
+
+    return hof[0]
+
+# Виклик генетичного алгоритму
+best_individual = genetic_algorithm()
+print(f"Best individual: {best_individual}, Fitness: {eval_func(best_individual)}")
